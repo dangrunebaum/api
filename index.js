0355046838c789f2daa6c8dc663215c32e87f693
daniel grunebaum <grund302@newschool.edu>
2025-06-22 00:42:12 -0400

    add package.json

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('API is running!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
